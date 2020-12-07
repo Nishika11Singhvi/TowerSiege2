3aa5b3ede315a72d9fb41f0e5751661534e123fa
Nishika11Singhvi <73836493+Nishika11Singhvi@users.noreply.github.com>
2020-12-07 21:22:24 +0530

    Add files via upload

---
+++ Box5.js
@@ -0,0 +1,26 @@
+class Box5 extends BaseClass 
+{
+  constructor(x, y, width, height)
+  {
+    super(x,y,width,height);
+    this.image = loadImage("r5.png");
+  }
+
+  display()
+  {
+    if(this.body.speed<5)
+    {
+      super.display();
+    }
+    else
+    {
+      World.remove(world,this.body);
+
+      push();
+      this.visibility=this.visibility-1;
+      tint(255,this.visibility);
+      image(this.image,this.body.position.x,this.body.position.y,this.width, this.height);
+      pop();
+    }
+  }
+};
